25642483051020cc8b46993b914204823e463fce
Jeff CYT <yutingjeffchen@gmail.com>
2023-05-04 15:25:03 +0800

    Include language specifying files

--- src/extension.ts
+++ src/extension.ts
@@ -16,7 +16,7 @@ export function activate(context: vscode.ExtensionContext) {
 	let disposable = vscode.commands.registerCommand('gb-mode.helloWorld', () => {
 		// The code you place here will be executed every time your command is executed
 		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from gb-mode!');
+		vscode.window.showInformationMessage('gb-mode for guarded command language GuaBao');
 	});
 
 	context.subscriptions.push(disposable);
